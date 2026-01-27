97704fc95904773a68ba9869f2e3d648803e124e
Matthew Valdez <info@matthewvaldez.com>
2026-01-26 20:16:33 -0800

    First draft of portfolio

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+// footer.php
+$year = date('Y');
+?>
+
+</main>
+
+<footer class="site-footer">
+    <p>&copy; <?php echo $year; ?> Matthew Valdez</p>
+</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,44 @@
+<?php
+// header.php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Matthew Valdez</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- font stuff -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,400;500;600&family=Fraunces:opsz,wght@9..144,400;500;600&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+
+  <!-- bootstrap stuff -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  
+  <link rel="stylesheet" href="/css/styles.css">
+
+  <!-- favicon stuff -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/img/fav/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/img/fav/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/img/fav/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+
+
+</head>
+<body>
+
+<header class="site-header">
+  <nav class="nav">
+    <a href="#home"><i class="bi bi-house-door"></i> Home</a>
+    <a href="#about"><i class="bi bi-file-earmark-person"></i> About</a>
+    <a href="#portfolio"><i class="bi bi-journal-check"></i> Portfolio</a>
+    <a href="#contact"><i class="bi bi-pencil"></i> Contact</a>
+  </nav>
+</header>
+
+<main>
